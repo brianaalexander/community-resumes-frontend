03db635e26b3b655f6716e6aac11764aa9ab3e19
rivkicobersy <rcobersy@gmail.com>
2022-11-03 03:15:32 +0200

    show action added

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,15 +1,19 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
 import { UsersIndex } from "./UsersIndex";
+import { UsersShow } from "./UsersShow";
+import { Modal } from "./Modal";
 
 export function Home() {
   const [users, setUsers] = useState([]);
+  const [isUsersShowVisible, setIsUsersShowVisible] = useState(false);
+  const [currentUser, setCurrentUser] = useState({});
 
   const handleIndexUsers = () => {
     // console.log("handleIndexUsers");
     // axios.get("http://localhost:3000/users.json").then((response) => {
     //   console.log(response.data);
-    //   setPhotos(response.data);
+    //   setUsers(response.data);
     // });
     setUsers([
       {
@@ -19,6 +23,7 @@ export function Home() {
         image_url:
           "https://www.simplebooks.co.za/wp-content/uploads/2019/07/canstockphoto22402523-arcos-creator.com_-1024x1024.jpg",
         personal_website_url: "test.com",
+        linledin_url: "linkedin.com",
       },
       {
         id: 1,
@@ -27,15 +32,30 @@ export function Home() {
         image_url:
           "https://www.simplebooks.co.za/wp-content/uploads/2019/07/canstockphoto22402523-arcos-creator.com_-1024x1024.jpg",
         personal_website_url: "test.com",
+        linledin_url: "linkedin.com",
       },
     ]);
+  };
+
+  const handleShowUser = (user) => {
+    console.log("handleShowUser", user);
+    setIsUsersShowVisible(true);
+    setCurrentUser(user);
+  };
+
+  const handleHideUser = () => {
+    console.log("handleHideUser");
+    setIsUsersShowVisible(false);
   };
 
   useEffect(handleIndexUsers, []);
 
   return (
     <div>
-      <UsersIndex users={users} />
+      <UsersIndex users={users} onSelectUser={handleShowUser} />
+      <Modal show={isUsersShowVisible} onClose={handleHideUser}>
+        <UsersShow user={currentUser} />
+      </Modal>
     </div>
   );
 }
