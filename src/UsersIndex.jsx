92077d9aedfd6f09fcd0f57c7e0d4723e364d376
brianaalexander <bribri0283@gmail.com>
2022-11-02 20:18:38 -0500

    added bootstrap and search function

--- src/UsersIndex.jsx
+++ src/UsersIndex.jsx
@@ -1,16 +1,44 @@
+import { useState } from "react";
+
 export function UsersIndex(props) {
+  const [searchFilter, setSearchFilter] = useState("");
+  console.log(props);
+
   return (
-    <div>
-      <h1>All users</h1>
-      {props.users.map((user) => (
-        <div key={user.id}>
-          <h2>
-            {user.first_name} {user.last_name}
-          </h2>
-          <img src={user.image_url} />
-          <p>{user.personal_website_url}</p>
-        </div>
-      ))}
+    <div id="users-index">
+      <h1>All Users</h1>
+      Search filter:{" "}
+      <input
+        value={searchFilter}
+        onChange={(event) => setSearchFilter(event.target.value)}
+        type="text"
+        list="first_name"
+      />
+      <datalist id="first_name">
+        {props.users.map((user) => (
+          <option key={user.id}>{user.first_name}</option>
+        ))}
+      </datalist>
+      <div className="row">
+        {props.users
+          .filter((user) => user.first_name.toLowerCase().includes(searchFilter.toLowerCase()))
+          .map((user) => (
+            <div className="col-md-3 mb-4" key={user.id}>
+              <div className="card">
+                <img src={user.image_url} className="card-img-top" />
+                <div className="card-body">
+                  <h2 className="card-name">
+                    {user.first_name} {user.last_name}
+                  </h2>
+                  <p className="card-text">{user.personal_website_url}</p>
+                  <a className="btn btn-primary" onClick={() => props.onSelectUser(user)}>
+                    More Info
+                  </a>
+                </div>
+              </div>
+            </div>
+          ))}
+      </div>
     </div>
   );
 }
