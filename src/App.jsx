452c2f0607f45b7c8ed15a6f6c9062660e6266e0
rivkicobersy <rcobersy@gmail.com>
2022-11-04 03:22:47 +0200

    connected to backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
+import { useState, useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Header } from "./Header";
 import { Home } from "./Home";
 import { Footer } from "./Footer";
+import { UsersShow } from "./UsersShow";
 
 function App() {
+  const [isUsersShowVisible, setIsUsersShowVisible] = useState(false);
+  const [currentUser, setCurrentUser] = useState({});
+
   return (
     <div>
-      <Header />
-      <Home />
-      <Footer />
+      <BrowserRouter>
+        <Header />
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/:id}" element={<UsersShow />} />
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
